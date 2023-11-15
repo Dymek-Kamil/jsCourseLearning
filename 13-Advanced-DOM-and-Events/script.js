1151c0d923ddc8b8ed45e5a79358d0e66a716515
Dymek-Kamil <kamil.dymek33@gmail.com>
2023-11-15 13:47:20 +0100

    Building a Tabbed Component

--- 13-Advanced-DOM-and-Events/script.js
+++ 13-Advanced-DOM-and-Events/script.js
@@ -74,6 +74,30 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clasue
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+
+  // Active content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
 // // Selecting, Creating and Deleting Elements
 // console.log(document.documentElement);
 // console.log(document.head);
@@ -230,5 +254,5 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 // console.log(h1.parentElement.children);
 // [...h1.parentElement.children].forEach(function (el) {
-//   if (el !== h1) el.style.transform = 'scale(0.5)';
+// if (el !== h1) el.style.transform = 'scale(0.5)';
 // });
