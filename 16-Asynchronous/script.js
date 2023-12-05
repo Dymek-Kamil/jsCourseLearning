f3a56a1fc3113cdd32c876d83ff27d37f776c9d0
Dymek-Kamil <kamil.dymek33@gmail.com>
2023-12-05 16:43:53 +0100

    Throwing Errors Manually

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -115,18 +115,27 @@ const renderCountry = function (data, className = '') {
 //       renderCountry(data[0]);
 //     });
 // };
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
 
 const getCountryData = function (country) {
   // Country 1
-  fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(response => response.json())
+  getJSON(`https://restcountries.com/v2/name/${country}`, 'Country not found')
     .then(data => {
       renderCountry(data[0]);
       const neighbour = data[0].borders?.[0];
+      //   const neighbour = 'dsadfas';
       // Country 2
-      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+      return getJSON(
+        `https://restcountries.com/v2/alpha/${neighbour}`,
+        'Country not found'
+      );
     })
-    .then(response => response.json())
     .then(data => renderCountry(data, 'neighbour'))
     .catch(err => {
       console.error(`${err} 💥💥💥`);
@@ -138,5 +147,6 @@ const getCountryData = function (country) {
 };
 
 btn.addEventListener('click', function () {
-  getCountryData('asd');
+  getCountryData('denmark');
 });
+getCountryData('iceland');
