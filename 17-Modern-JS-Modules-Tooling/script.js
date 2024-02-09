5865601d20ffd290380175482a89c8fd2fb9fa0c
Dymek-Kamil <kamil.dymek33@gmail.com>
2024-02-10 00:36:26 +0100

    Exporting and Importing in ES6 Modules

---
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -0,0 +1,48 @@
+// Importing module
+// import {
+//   addToCart,
+//   totalQuantity as tq,
+//   totalprice as price,
+// } from './shoppingCart.js';
+// addToCart('bread', 5);
+// console.log(price, tq);
+
+console.log(`Importing Module`);
+// console.log(shippingCost);
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// import add, {
+//   addToCart,
+//   totalQuantity as tq,
+//   totalPrice as price,
+// } from './shoppingCart.js';
+
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
+
+console.log(cart);
+
+// console.log(`Start fetching`);
+// const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+// const data = await res.json();
+// console.log(data);
+// console.log(`Something`);
+
+const getLastPost = async function () {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
